71bfeeefe9cae0a1bd284f93be84db2ae8a6d3ef
arlsr <invalid@email>
2015-07-26 23:09:54 +0100

    Forward movement in imaginary grid.

--- Classes/Ship.cpp
+++ Classes/Ship.cpp
@@ -4,7 +4,8 @@
 
 Ship::Ship(cocos2d::Label *lblCoords)
 {
-	rotatingLeft = rotatingRight = false;
+	rotatingLeft = rotatingRight = propellingForward = false;
+	worldX = worldY = 0;
 
 	this->lblCoords = lblCoords;
 	this->initWithFile("Ship.png");
@@ -31,6 +32,8 @@ void Ship::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event*
 		this->rotatingLeft = true;
 	else if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_RIGHT_ARROW)
 		this->rotatingRight = true;
+	else if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_UP_ARROW)
+		this->propellingForward = true;
 }
 
 void Ship::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event)
@@ -39,12 +42,38 @@ void Ship::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event
 		this->rotatingLeft = false;
 	else if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_RIGHT_ARROW)
 		this->rotatingRight = false;
+	else if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_UP_ARROW)
+		this->propellingForward = false;
+}
+
+void Ship::stepForward(float delta) {
+
+	cocos2d::Vec2 direction, velocity;
+	float rotation = this->getRotation();
+	float speed = 100.0;
+	float rotationRadians = rotation * (M_PI / 180);
+
+	direction.x = cos(rotationRadians);
+	direction.y = sin(rotationRadians);
+	if (direction.length() > 0) {
+		direction.normalize();
+	}
+	//Then scale it by the current speed to get the velocity
+	velocity.x = direction.x * speed;
+	velocity.y = direction.y * speed;
+
+	//So now you know your velocity based on your rotation.You can the update your position with that information.
+
+	//Update the position based on our current speed
+	//This is basic s = vt physics
+	worldX += velocity.x * delta;
+	worldY += velocity.y * delta;
 }
 
 void Ship::update(float delta) {
 	
 	std::ostringstream os;
-	os << "x: " << this->getPositionX() << ", y:" << this->getPositionY();
+	os << "x: " << worldX << ", y:" << worldY;
 	this->lblCoords->setString(os.str());
 
 	if (rotatingLeft) {
@@ -52,5 +81,14 @@ void Ship::update(float delta) {
 	}
 	if (rotatingRight) {
 		this->setRotation(this->getRotation() + (60 * delta));
+	}
+	// HACK: This has to be the wrong way to change a sprite.
+	if (propellingForward) {
+		this->initWithFile("Ship_fwd.png");
+
+		stepForward(delta);
+	}
+	else {
+		this->initWithFile("Ship.png");
 	}
 }
--- Classes/Ship.h
+++ Classes/Ship.h
@@ -8,11 +8,15 @@ private:
 	cocos2d::Label *lblCoords;
 	bool rotatingLeft;
 	bool rotatingRight;
+	bool propellingForward;
+	int worldX;
+	int worldY;
 public:
 	Ship(cocos2d::Label *lblCoords);
 	~Ship();
 	void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
 	void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
+	void stepForward(float delta);
 	void update(float delta) override;
 };
 
